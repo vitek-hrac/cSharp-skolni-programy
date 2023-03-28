f6e6b4ebe641f2861da9d0b6912712d42814d95f
vitek-hrac <120108993+vitek-hrac@users.noreply.github.com>
2023-03-28 14:09:09 +0200

    pocet jablek a hrusek
    Uživatel zadá pocet jablek a hrusek a program spočítá počet šťáv

---
+++ Cerny-vino.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //promenne
+            string jablkoString, hrozenString;
+            double jablka, hrozen, jableMost, vino,celkem;
+
+            //zadavani hodnot
+            Console.WriteLine("pro vynechani ovoce zadej 0\nzadej pocet jablek v kg: ");
+            jablkoString = Console.ReadLine();
+            double.TryParse(jablkoString, out jablka);
+
+            Console.WriteLine("zadej pocet hroznu v kg: ");
+            hrozenString = Console.ReadLine();
+            double.TryParse(hrozenString, out hrozen);
+
+            //pocitani hodnot
+            jableMost= jablka*0.6;
+            vino = hrozen * 0.85;
+            celkem = hrozen + jablka;
+
+            //vypis vypocitanych hodnot
+            Console.WriteLine("\nPocet hroznu v kg: "+ hrozenString);
+            Console.WriteLine("Pocet jablek v kg: "+ jablkoString);
+            Console.WriteLine("\nLitru jablecneho mostu: "+ jableMost);
+            Console.WriteLine("Litru vina: "+ vino);
+            Console.WriteLine("\nCelkem bylo " + celkem + "kg ovoce");
+            celkem = jableMost + vino;
+            Console.WriteLine("\nCelkem bylo " + celkem + "l stavy");
+
+            Console.Read();
+        }
+    }
+}
